58fed6f2e70092b8cdc52e1c7b30ab75cf2e015e
Tatsuya-2066 <yokotatsukaisinn@gmail.com>
2023-01-13 12:58:49 +0900

    テスト用のchurch_impulse.wavを用意してクリック時に音がなるようにした

--- Assets/Script/PunchGage.cs
+++ Assets/Script/PunchGage.cs
@@ -10,14 +10,22 @@ public class PunchGage : MonoBehaviour
     float upDown = 1; // 得点を代入する変数
     public BreakObject BreakObject;
     public lifeGage lifeGage;
-    
+
+    public GameObject clicked_object;
+    public AudioClip clip; // AudioSourceのAudioClipに選択されている音データを格納
+
     public void punch(){
         if(lifeGage.damege(totalScore)==1){
           upDown=0;
         }else{
             upDown=1;
         }
-   }
+        clicked_object = GameObject.Find("tsuboObjet");
+        clip = clicked_object.GetComponent<AudioSource>().clip;
+        Debug.Log(clip);
+        clicked_object.GetComponent<AudioSource>().PlayOneShot(clip);
+        Debug.Log("You clicked.");
+    }
     void Update()
     {
         scoreGauge.fillAmount = totalScore / 100.0f;
