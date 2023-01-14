3aeef8a20574a12ea448ee9cb59ad9892a12262a
saint828 <f5420079@stu.chs.nihon-u.ac.jp>
2023-01-14 17:27:17 +0900

    オブジェクト名を訂正

--- Assets/Script/PunchGage.cs
+++ Assets/Script/PunchGage.cs
@@ -21,7 +21,7 @@ public class PunchGage : MonoBehaviour
         }else{
             upDown=1;
         }
-        clicked_object = GameObject.Find("tsuboObjet");
+        clicked_object = GameObject.Find("tsuboObjcet");
         clip = clicked_object.GetComponent<AudioSource>().clip;
         Debug.Log(clip);
         clicked_object.GetComponent<AudioSource>().PlayOneShot(clip);
