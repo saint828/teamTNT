c23cf8d05bba250fb4c8201628bd8f84ce3d96db
saint828 <f5420079@stu.chs.nihon-u.ac.jp>
2023-01-19 09:34:41 +0900

    LifeGageを再生成

---
+++ Assets/Script/LifeGage.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // <--忘れがち
+
+public class LifeGage : MonoBehaviour
+{
+    public Image scoreGauge; // ゲージ本体画像をインスペクターからセット
+    private float totalScore=75.0f; // 得点を代入する変数
+    private float maxLife=100.0f; // 得点を代入する変数
+    public void reset(float firstLife)
+    {
+        totalScore=firstLife;
+        maxLife=firstLife*3/4;
+    }
+    public int damage(float damege){
+        totalScore-=damege*0.3f;
+        if(totalScore<0){
+            totalScore=0;
+            scoreGauge.fillAmount = totalScore / maxLife;
+            return 1;
+        }
+        scoreGauge.fillAmount = totalScore / maxLife;
+        return 0;
+    }
+}
